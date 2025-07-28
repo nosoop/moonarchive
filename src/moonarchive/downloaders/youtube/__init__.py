581835ae11790304ceb318a34e7633671f7189d1
nosoop <nosoop@users.noreply.github.com>
2025-07-28 02:56:17 -0700

    Add xerror flag to ffmpeg
    -xerror stops the ffmpeg process and exits on errors.
    
    It turns out that ffmpeg's default configuration may silently
    accept major system issues like I/O errors (which I happened to
    discover while running a test instance of moombox on a failing
    hard disk).
    
    We also add a bitstream filter to ensure that the DTS is
    monotonically increasing to process the error case before ffmpeg
    does (and with -xerror, explodes).
    
    I'm still not completely sure if there's any errors that we
    shouldn't catch, so for now this goes into staging while it
    undergoes additional testing.
    
    Closes #11.

--- src/moonarchive/downloaders/youtube/__init__.py
+++ src/moonarchive/downloaders/youtube/__init__.py
@@ -636,6 +636,7 @@ async def _run(args: "YouTubeDownloader") -> None:
             "-progress",
             "-",
             "-nostdin",
+            "-xerror",
             "-y",
         ]
 
@@ -645,6 +646,8 @@ async def _run(args: "YouTubeDownloader") -> None:
                 "0",
                 "-thread_queue_size",
                 "1024",
+                "-bsf",
+                "setts=dts='max(PREV_OUTDTS+1,DTS)'",  # prevent non-monotonic DTS errors
                 "-i",
                 str(output_stream_path.absolute()),
             )
