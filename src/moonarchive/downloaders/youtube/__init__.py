d2dfc1354d118427873f18e596be5483d929b164
nosoop <nosoop@users.noreply.github.com>
2025-08-17 08:36:37 -0700

    Move JSON extraction to separate file

--- src/moonarchive/downloaders/youtube/__init__.py
+++ src/moonarchive/downloaders/youtube/__init__.py
@@ -5,7 +5,6 @@ import collections
 import dataclasses
 import datetime
 import hashlib
-import html.parser
 import io
 import itertools
 import json
@@ -28,6 +27,7 @@ import msgspec
 from ...models import messages as messages
 from ...models.ffmpeg import FFMPEGProgress
 from ...output import BaseMessageHandler
+from ._extract import PlayerResponseExtractor, YTCFGExtractor
 from .config import YTCFG
 from .player import (
     YTPlayerAdaptiveFormats,
@@ -94,38 +94,6 @@ def _string_byte_trim(input: str, length: int) -> str:
     except UnicodeDecodeError as err:
         return bytes_[: err.start].decode()
 
-
-def create_json_object_extractor(decl: str) -> Type[html.parser.HTMLParser]:
-    class InternalHTMLParser(html.parser.HTMLParser):
-        in_script: bool = False
-        result = None
-
-        def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
-            self.in_script = tag == "script"
-
-        def handle_endtag(self, tag: str) -> None:
-            self.in_script = False
-
-        def handle_data(self, data: str) -> None:
-            if not self.in_script:
-                return
-
-            decl_pos = data.find(decl)
-            if decl_pos == -1:
-                return
-
-            # we'll just let the decoder throw to determine where the data ends
-            start_pos = data[decl_pos:].find("{") + decl_pos
-            try:
-                self.result = json.loads(data[start_pos:])
-            except json.JSONDecodeError as e:
-                self.result = json.loads(data[start_pos : start_pos + e.pos])
-
-    return InternalHTMLParser
-
-
-PlayerResponseExtractor = create_json_object_extractor("var ytInitialPlayerResponse =")
-YTCFGExtractor = create_json_object_extractor('ytcfg.set({"CLIENT')
 
 # initial innertube data; these should be replaced by the results from extract_yt_cfg
 _INITIAL_INNERTUBE_CLIENT_CONTEXT = {
